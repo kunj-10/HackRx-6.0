149dd299e4591a1d8ff2b3dec275d0a886f9f996
luv29 <luvkansal29@gmail.com>
2025-07-31 19:25:55 +0530

    Some changes

--- app/contextual retrieval/context_appender.py
+++
@@ -1,8 +0,0 @@
-# context_appender.py
-# This module appends relevant context from vector search to the user query.
-
-from typing import List
-
-def append_context_to_query(query: str, retrieved_chunks: List[str]) -> str:
-    context = "\n\n".join(retrieved_chunks)
-    return f"Context:\n{context}\n\nQuery:\n{query}"
