3778aa651e5a5c8c9a69c59ca6cb68ef0397e949
luv29 <luvkansal29@gmail.com>
2025-08-03 14:05:22 +0530

    refinements

--- run.py
+++ run.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=int(os.getenv("PORT", "8080")),
+        port=int(os.getenv("PORT", "8000")),
         reload=True,
         log_level="info"
     ) 
