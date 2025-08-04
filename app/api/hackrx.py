5c41ba15df4d48b31a6991990d8b0016cba93cb0
kunj_10 <kunjvgoyal30@gmail.com>
2025-08-04 17:57:45 +0530

    Cleaning File Structure

--- app/api/hackrx.py
+++ app/api/hackrx.py
@@ -1,12 +1,8 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel, HttpUrl
-from app.utils import extract_text
-from urllib.parse import urlparse
+from app.utils import extract_text, save_file_from_url
 from typing import List
-import aiofiles
 import logging
-import aiohttp
-import uuid
 import time
 import os
 import asyncio
@@ -14,16 +10,13 @@ from app.services.vector_store_service import process_and_store_document
 from app.services.agent import pdf_ai_expert
 from app.services.rag import answer_query
 from app.utils import compute_sha256
-from app.api.db.mongo import file_collection
+from app.db.mongo import file_collection
 
 hackrx_router = APIRouter()
 
 class HackRxRequest(BaseModel):
     documents: HttpUrl
     questions: List[str]
-
-DOWNLOAD_DIR = "data"
-os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 @hackrx_router.post('/hackrx/run')
 async def run_hackrx(
@@ -33,24 +26,14 @@ async def run_hackrx(
     try:
         start_time = time.monotonic()
 
-        parsed_url = urlparse(str(payload.documents))
-        original_filename = f"{uuid.uuid4()}_{os.path.basename(parsed_url.path)}"
-        if not original_filename:
-            raise HTTPException(status_code=400, detail="Could not determine filename from URL")
+        filepath, original_filename = save_file_from_url(payload.documents)
 
-        filepath = os.path.join(DOWNLOAD_DIR, original_filename)
-
-        async with aiohttp.ClientSession() as session:
-            async with session.get(str(payload.documents)) as response:
-                if response.status != 200:
-                    raise HTTPException(status_code=400, detail="Failed to download file")
-
-                async with aiofiles.open(filepath, 'wb') as f:
-                    await f.write(await response.read())
         before_hash = time.monotonic()
         file_hash = await compute_sha256(filepath)
         after_hash = time.monotonic()
+        
         logging.info(f"Time taken to hash: {(after_hash-before_hash):.2f} seconds")
+
         existing = await file_collection.find_one({"hash": file_hash})
         if existing:
             logging.info(f"File already processed: {existing['filename']}")
@@ -61,6 +44,8 @@ async def run_hackrx(
             await file_collection.insert_one({"hash": file_hash, "filename": original_filename})
             filename=original_filename
             logging.info("File Processed")
+
+        
         # filename=original_filename
 
         # text = extract_text(filepath)
@@ -70,6 +55,7 @@ async def run_hackrx(
         response['answers'] = await asyncio.gather(*[
             answer_query(question, filename) for question in payload.questions
         ])
+        
         # for question in payload.questions:
             # result = await pdf_ai_expert.run(f"source_file is {original_filename}. user_query: {question}")
 
--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -2,6 +2,7 @@ from app.utils.prompts import *
 from app.utils.auth import verify_token
 from .extract_text import extract_text
 from .hash import compute_sha256
+from .file_handling import save_file_from_url
 
 __all__ = [
     RAG_AGENT_SYSTEM_PROMPT
